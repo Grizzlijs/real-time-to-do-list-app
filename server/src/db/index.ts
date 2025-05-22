3449d5bfbfdc07b0de4959f62c796dbd67c83d0b
Emils Samoilovs <samoilovs.emils@gmail.com>
2025-05-22 18:57:27 +0300

    Setup for production

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: process.env.PG_USER || 'postgres',
   password: process.env.PG_PASSWORD || 'postgres',
   host: process.env.PG_HOST || 'localhost',
-  port: parseInt(process.env.PG_PORT || '5432'),
+  port: parseInt(String(process.env.PG_PORT || '5432')),
   database: process.env.PG_DATABASE || 'todo_app'
 });
 
