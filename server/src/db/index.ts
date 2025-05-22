f10bebe3fd41d05ff3b4bd2af7cd543a2e5b615f
Emils Samoilovs <samoilovs.emils@gmail.com>
2025-05-22 18:52:12 +0300

    Setup for production

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: parseInt(process.env.PG_PORT),
-  database: process.env.PG_DATABASE
+  user: process.env.PG_USER || 'postgres',
+  password: process.env.PG_PASSWORD || 'postgres',
+  host: process.env.PG_HOST || 'localhost',
+  port: parseInt(process.env.PG_PORT || '5432'),
+  database: process.env.PG_DATABASE || 'todo_app'
 });
 
 export default pool;
